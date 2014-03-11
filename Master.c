3ae99733ab28debe46a1ddec593311626b20c595
Alexandre Lupu <alexandre.lupu@etu.univ-orleans.fr>
2014-03-11 17:29:18 +0100

    creation des structures de données pour stocker les différents agents

--- Master.c
+++ Master.c
@@ -13,14 +13,24 @@
 #include <time.h>
 
 struct spy_t{
-  int id;
-  
-}
+  char * hote;
+  int sock;
+};
 
 struct controleur_t{
   int numSalle;
-  spy_t * lesSpy;
+  int sock;
+  struct spy_t * lesSpy;
+  pthread_mutex_t *verrou;
+  pthread_t th;
 };
+
+int lireEntier(int client){
+  char c; //c initialisé a n'importe quoi sauf \n
+  printf("%d\n", c);
+  read(client, &c, 1);
+  return atoi(msg);
+}
 
 int initSocketServeur(short port){
   int sock, val;
@@ -48,7 +58,20 @@ int initSocketServeur(short port){
   return sock;
 }
 
-void * gereConnection(){}
+void * gereObserveur(){
+  
+}
+
+void * gereControleur(){
+  
+}
+
+void * gereConnection(){
+  int codeClient=lireEntier(sock); //recupere le type du client
+  
+  if(codeClient==0) gereControleur();
+  if(codeClient)
+    }
 
 int main(int args, char *arg[]){
   //initialisation du serveur
@@ -58,19 +81,21 @@ int main(int args, char *arg[]){
   pthread_t th;
   sock=initSocketServeur(atoi(arg[1]));
   if (sock==-1) return -1; //en cas d'echec de l'initialisation
-
+  
   //variables pour accepter une connexion
   struct sockaddr_in stclient; //ma structure d'adresse pour le client
-
+  
   socklen_t taille=sizeof(struct sockaddr_in);
-
+  
   int client; //descripteur pour le client  
-
+  
+  
   //attente de clients puis gestion des requêtes
   while (1) {
+    
     client=accept(sock, (struct sockaddr *) &stclient, &taille);
     pthread_create(&th, NULL, gereConnection, NULL);
-
+    
   }
   return 0;
 }
