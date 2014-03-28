41c4b7932f1c77952aa122b8e7f84d1134805efe
Martin_Julien <julien.martin@etu.univ-orleans.fr>
2014-03-28 20:49:02 +0100

    Modification du Spy avec des thread et un Alerte continue sur le spy

--- Spy.c
+++ Spy.c
@@ -9,6 +9,24 @@
 #include <string.h>
 #include <netdb.h>
 
+
+struct paramA{         //Struct pour le thread de Alerte
+  int  sock;
+  int  nb_processus;
+  char ** processus;
+};
+
+struct paramB{         //Struct pour le thread de Controle
+  int  sock;
+  char * com;
+};
+
+struct paramC{         //Struct pour le thread de Message
+  char * mes;
+};
+
+pthread_mutex_t verrou;
+
 int initSocketClient(char *host, short port){
   int sock, val;
   struct sockaddr_in serv; //ma structure d'adresse pour le serveur
@@ -39,66 +57,89 @@ int initSocketClient(char *host, short port){
 //Screen: DISPLAY=:0.0 import -window root screenshot.jpg
 
 
-int Alerte(int sock,char * processus[],int nb_processus){
-  write(sock,"A",1);
-  int alerte=1;  //Tant que c'est vrai on continue la recherche
-  int j;
-  int i=0;     //Pour parcourir le buf lors du fgets
-  FILE * fp;   //Pour contenir le popen
-  char * msg;  //Pour contenir la commande qui selectionne la liste des processus
-  char buf[5]; //Pour lire le résultat du fgets
+void * Alerte(void *par){
+  
+  struct paramA *argj=(struct paramA *)par;
+
+  int alerte=1;    //Tant que c'est vrai on continue la recherche
+  int j,h,ok=1;    //Initialisation de variable pour les boucles i et j , et ok pour le booleen
+  int i=0;         //Pour parcourir le buf lors du fgets
+  FILE * fp;       //Pour contenir le popen
+  char * msg;      //Pour contenir la commande qui selectionne la liste des processus
+  char buf[5];     //Pour lire le résultat du fgets
+
+  int actif[(*argj).nb_processus];
 
   while(alerte){
-    for(j=0 ;j<nb_processus ; j++){
-      asprintf(&msg,"ps -e | grep -w '%s' | cut -d\":\" -f3 | cut -d\" \" -f2 | wc -l",processus[j]);
+    sleep(30);
+    pthread_mutex_lock(&verrou);
+    //write((*argj).sock,"A",1);
+    for(j=0 ;j<(*argj).nb_processus ; j++){
+      asprintf(&msg,"ps -e | grep -w '%s' | cut -d\":\" -f3 | cut -d\" \" -f2 | wc -l",(*argj).processus[j]);
       
       fp=popen(msg,"r");
-      
-      if (fp == NULL) return -1;
-      
-      while( fgets(buf,sizeof buf,fp) != NULL) {
+           
+      while( fgets(buf,sizeof buf,fp) != NULL && ok) {
 	if(buf[i]!='0'){
-	  //printf("%s \n",processus[j]);
-	  alerte=0;
-	  write(sock,processus[j],sizeof(processus[j]));
-	  j=nb_processus;
+	  actif[j]=1;
+	  ok=0;
+	}
+	else{
+	  actif[j]=0;
 	}
 	i++;
       }
+      ok=1;
       i=0;
       pclose(fp);
     }
     j=0;
+    for(h=0;h<(*argj).nb_processus;h++){
+      if(actif[h]){
+	printf("%i",actif[h]);
+	write((*argj).sock,"A",1);
+	write((*argj).sock,(*argj).processus[h],sizeof((*argj).processus[h]));
+      }
+    }
+    pthread_mutex_unlock(&verrou);
   }
 }
 
 
-int Controle(int sock,char * com){
-  write(sock,"C",1);            //On écrit un C pour être dans la partie controle dans le Controleur.c
+void * Controle(void *par){
+
+  struct paramB *argj=(struct paramB *)par;
+
+  pthread_mutex_lock(&verrou);
+  write((*argj).sock,"C",1);               //On écrit un C pour être dans la partie controle dans le Controleur.c
   FILE * pf;
   char * msg;
 
-  asprintf(&msg,"%s",com);        // On execute la commande en parametre
-  pf = popen(msg,"r");            // On recupére le résultat en popen dans pf
-  if (pf == NULL) return -1;
+  asprintf(&msg,"%s",(*argj).com);         // On execute la commande en parametre
+  pf = popen(msg,"r");                     // On recupére le résultat en popen dans pf
+  //if (pf == NULL) return -1;
    
-  char * str2=malloc(sizeof(*str2));  //On alloue la mémoire de str2 pour pouvoir récuperer mot à mot les résultats
-  char * str3="";                     //str3 contient le résultat
+  char * str2=malloc(sizeof(*str2));       //On alloue la mémoire de str2 pour pouvoir récuperer mot à mot les résultats
+  char * str3="";                          //str3 contient le résultat
 
-  while(fscanf(pf,"%s",str2) != EOF){    //Tant que le fichier pf n'est pas vide
-    asprintf(&str3,"%s %s",str3,str2);   //On récupere le résultat de la commande
+  while(fscanf(pf,"%s",str2) != EOF){      //Tant que le fichier pf n'est pas vide
+    asprintf(&str3,"%s %s",str3,str2);     //On récupere le résultat de la commande
   }
 
-  write(sock,str3,strlen(str3));   //On renvoit le résultat de la commande
+  write((*argj).sock,str3,strlen(str3));   //On renvoit le résultat de la commande
 
   pclose(pf);
+  pthread_mutex_unlock(&verrou);
 }
 
 
-void Message(int sock,char * mes){
-  //write(sock,"M",1);
-  //write(sock,mes,strlen(mes));
-  printf("%s",mes);
+void * Message(void *par){
+
+  struct paramC *argj=(struct paramC *)par;
+
+  pthread_mutex_lock(&verrou);
+  printf("%s",(*argj).mes);
+  pthread_mutex_unlock(&verrou);
 }
 
 void Visuelle(int sock){
@@ -110,17 +151,45 @@ int main(int args, char *arg[]){
   //initialisation du serveur
   int sock;
   sock=initSocketClient(arg[1], atoi(arg[2]));
-  if (sock==-1) return -1; //en cas d'echec de l'initialisation
+  if (sock==-1) return -1;              //en cas d'echec de l'initialisation
   
   char * processus[10];
   processus[0]="firefox";
   processus[1]="evince";
-  int nb_processus=2;   //Pour avoir le nombre de case utilisé dans le tableau de processus
-  //Si tu veux ajouter des processus oublie pas d'incrementer
+  processus[2]="emacs";
+  int nb_processus=3;                   //Pour avoir le nombre de case utilisé dans le tableau de processus
+                                        //Si tu veux ajouter des processus oublie pas d'incrementer
+
+  pthread_t th1,th2,th3;
+
+  struct paramA *pa=(struct paramA *)malloc(sizeof(struct paramA));
+  (*pa).sock=sock; 
+  (*pa).nb_processus=nb_processus;
+  (*pa).processus=processus;
+
+  char * com="date";
+
+  struct paramB *pb=(struct paramB *)malloc(sizeof(struct paramB));
+  (*pb).sock=sock;
+  (*pb).com=com;
+
+  char * mes="Ceci est le test de la fonction Message \n";
+
+  struct paramC *pc=(struct paramC *)malloc(sizeof(struct paramC));
+  (*pc).mes=mes;
+
+  pthread_mutex_init(&verrou, NULL);
+
+  pthread_create(&th1, NULL,Alerte,(void *)pa);
+  pthread_create(&th2, NULL,Controle,(void *)pb);
+  pthread_create(&th3, NULL,Message,(void *)pc);
+
+  pthread_join(th3,NULL);
+  pthread_join(th2,NULL);
+  pthread_join(th1,NULL);
   
-  Alerte(sock,processus,nb_processus);
-  Controle(sock,"date");
-  Message(sock,"Ceci est le test de la fonction Message ! \n");
+  pthread_mutex_destroy(&verrou);
+
   close(sock);
   return 0;
 }
